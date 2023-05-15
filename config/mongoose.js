1c9a3d1f29b62c9ee52acb65c47da70e41f98147
sanket sayal <snktsayal@gmail.com>
2023-05-15 18:59:08 +0530

    second

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
+require('dotenv').config()
 
 
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/csvUploads');
+mongoose.connect(process.env.MONGODB_URL);
 
 const db = mongoose.connection;
 
